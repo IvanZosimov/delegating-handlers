adb4310768a8ec2fcab2433961f233e350388058
hippy <hippy2981@gmail.com>
2022-11-25 13:49:45 -0800

    DEBUG delay: Add delay tests

---
+++ tests/rm.DelegatingHandlersTest/TempTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using NUnit.Framework;
+
+namespace rm.DelegatingHandlersTest
+{
+	public class TempTests
+	{
+		[Test]
+		[TestCase(25)]
+		[TestCase(5)]
+		[TestCase(0)]
+		[TestCase(50)]
+		[TestCase(100)]
+		public async Task __(int delayInMilliseconds)
+		{
+			const int iterations = 10;
+			for (int i = 0; i < iterations; i++)
+			{
+				var stopwatch = Stopwatch.StartNew();
+				await Task.Delay(delayInMilliseconds).ConfigureAwait(false);
+				stopwatch.Stop();
+
+				Assert.GreaterOrEqual(stopwatch.ElapsedMilliseconds, delayInMilliseconds);
+			}
+		}
+	}
+}
